da2c0a69b157222d997fac5203111ab1437fa160
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-08-22 21:36:57 +0600

    ReactJS Tutorial - 24 - Component Updating Lifecycle Methods

--- src/components/LifeCycleA.js
+++ src/components/LifeCycleA.js
@@ -9,6 +9,7 @@ class LifeCycleA extends Component {
       name: "Tanzim",
     };
 
+    this.changeState = this.changeState.bind(this);
     console.log("LifeCycleA constructor - ", Date.now());
   }
 
@@ -21,11 +22,40 @@ class LifeCycleA extends Component {
     console.log("LifeCycleA componentDidMount - ", Date.now());
   }
 
+  shouldComponentUpdate() {
+    console.log("LifeCycleA shouldComponentUpdate - ", Date.now());
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("LifeCycleA getSnapshotBeforeUpdate - ", Date.now());
+    return null;
+  }
+
+  componentDidUpdate() {
+    console.log("LifeCycleA componentDidUpdate - ", Date.now());
+  }
+
+  componentWillUnmount() {
+    console.log("LifeCycleA componentWillUnmount - ", Date.now());
+  }
+
+  /* Error Boundary */
+  // static getDerivedStateFromError(error) {}
+  // componentDidCatch(error, info) {}
+
+  changeState() {
+    this.setState({
+      name: "Lupin",
+    });
+  }
+
   render() {
     console.log("LifeCycleA render - ", Date.now());
     return (
       <>
         <div>LifeCycleA</div>
+        <button onClick={this.changeState}>Change State</button>
         <LifeCycleB />
       </>
     );
--- src/components/LifeCycleB.js
+++ src/components/LifeCycleB.js
@@ -1,29 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class LifeCycleB extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            name: 'Tahmid'
-        }
+    this.state = {
+      name: "Tahmid",
+    };
 
-        console.log('LifeCycleB constructor - ', Date.now())
-    }
+    console.log("LifeCycleB constructor - ", Date.now());
+  }
 
-    static getDerivedStateFromProps(props, state) {
-        console.log('LifeCycleB getDerivedStateFromProps - ', Date.now())
-        return null
-    }
+  static getDerivedStateFromProps(props, state) {
+    console.log("LifeCycleB getDerivedStateFromProps - ", Date.now());
+    return null;
+  }
 
-    componentDidMount() {
-        console.log('LifeCycleB componentDidMount - ', Date.now())
-    }
-    
-    render() {
-        console.log('LifeCycleB render - ', Date.now());
-        return <div>LifeCycleB</div>
-    }
+  componentDidMount() {
+    console.log("LifeCycleB componentDidMount - ", Date.now());
+  }
+
+  shouldComponentUpdate() {
+    console.log("LifeCycleB shouldComponentUpdate - ", Date.now());
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("LifeCycleB getSnapshotBeforeUpdate - ", Date.now());
+    return null;
+  }
+
+  componentDidUpdate() {
+    console.log("LifeCycleB componentDidUpdate - ", Date.now());
+  }
+
+
+  componentWillUnmount() {
+    console.log("LifeCycleB componentWillUnmount - ", Date.now());
+  }
+  
+  /* Error Boundary */
+  // static getDerivedStateFromError(error) {}
+  // componentDidCatch(error, info) {}
+
+  render() {
+    console.log("LifeCycleB render - ", Date.now());
+    return <div>LifeCycleB</div>;
+  }
 }
 
-export default LifeCycleB
+export default LifeCycleB;
